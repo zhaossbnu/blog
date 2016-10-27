6c4136fa72f023162392cabb08fd86820803f204
zhaossbnu <zhaossbnu@mail.bnu.edu.cn>
2016-10-27 15:21:15 +0800

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -76,20 +76,10 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE':   'django.db.backends.postgresql_psycopg2',
-        'NAME':     'mysite',
-        'USER':     'postgres',
-        'PASSWORD': '123456',
-        'HOST':     'localhost',
-        'PORT':     '',
-    },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
